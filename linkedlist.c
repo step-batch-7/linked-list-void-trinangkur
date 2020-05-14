ece45a3a0ca025bb5fc947037155cdae9814daf3
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-05-14 15:18:12 +0530

    Added forEach funtion for linkedlist

--- linkedlist.c
+++ linkedlist.c
@@ -117,3 +117,11 @@ Element reduce(List_ptr list, Element context, Reducer reducer) {
   }
   return context;
 }
+
+void forEach(List_ptr list, ElementProcessor processor) {
+  Node_ptr p_walk = list->first;
+  while(p_walk != NULL) {
+    processor(p_walk->element);
+    p_walk = p_walk->next;
+  }
+}
